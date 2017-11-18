6927ce536425772ef6459768f12d63cd5a6117c1
Richard Diehl Martinez <richarddiehl@gmail.com>
2017-11-18 10:01:27 -0800

    updated Class construction for Clustering

--- clustering.py
+++ clustering.py
@@ -1,7 +1,7 @@
 '''
 Clustering.py
 -----------------------
-Input: Clustering.py receives a matrix of word frequency counts that have been
+Input: Cluster Class receives a matrix of word frequency counts that have been
 normalized by TSIDF (term frequency–inverse document frequency). The algorithm
 uses the sklearn package for the K-means clustering algorithm.
 '''
@@ -9,23 +9,33 @@ from sklearn.cluster import KMeans
 import numpy as np
 from tempfile import TemporaryFile
 
-DATA_FILE = 'frequency_matrix.npy'
 NUM_CLUSTERS = 20
 
-# Class should be able to read in a new input and add it to the current
+# Read in a new input and add it to the current
 # history of what has been clustered, should be able to recluster
 # and should be able to print out new recommendations for the
-# wine types
+wine types
 
+class Cluster(object):
+    def __init__(self):
+        self.num_clusters = NUM_CLUSTERS
+        self.freq_matrix = None
+        self.labels = None
 
-def main():
-    # Freq_Matrix is a matrix containing the normalized frequencies of the
-    # words in the wine reviews; each row represents one wine review
-    Freq_Matrix = np.load(DATA_FILE)
-    kmeans = KMEANS(n_clusters = NUM_CLUSTERS,random_state=0).fit(Freq_Matrix)
-    labels = kmeans.labels_ #labels represent the new wine categories that our cluster f
+    def load_data(self,data_file):
+        self.freq_matrix = np.load(data_file)
 
-    #kmeans.predict(array of word frequencies)
+    def create_clusters(self):
+        # Freq_Matrix is a matrix containing the normalized frequencies of the
+        # words in the wine reviews; each row represents one wine review
+        try:
+            kmeans = KMEANS(n_clusters = NUM_CLUSTERS,random_state=0).fit(self.freq_matrix)
+            self.labels = kmeans.labels_
+        except:
+            print "Data read-in error!"
 
-if __name__ == '__main__':
-    main()
+    def get_labels(self):
+        return self.labels
+
+    def cluster_assignment(self,new_data):
+        return kmeans.predict(new_data)
