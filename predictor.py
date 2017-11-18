633a78c4f730ac91e2cbc78b0e9d4f894838e884
Richard Diehl Martinez <richarddiehl@gmail.com>
2017-11-18 11:29:09 -0800

    created Predictor class

---
+++ predictor.py
@@ -0,0 +1,11 @@
+'''
+Predictor.py
+-----------------------
+Input: Cluster Class receives a matrix of word frequency counts that have been
+normalized by TSIDF (term frequency–inverse document frequency). The algorithm
+uses the sklearn package for the K-means clustering algorithm.
+'''
+
+class Predictor(object):
+    def __init__(self):
+        suck my ass!
