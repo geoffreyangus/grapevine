6532f4376d139214d6ff8518853479060df5b062
rdiehlmartinez <rdm@stanford.edu>
2017-12-10 03:31:21 -0800

    Add files via upload

--- feature_extraction.py
+++ feature_extraction.py
@@ -11,55 +11,82 @@ these into the frequency matrix.
 
 import numpy as np
 import json
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-CORPUS_FILE = 'word_corpus.txt'
-JSON_FILE = 'json.txt'
+CORPUS_FILE = 'data/word_corpus.txt'
 
 # reads in a corpus of english words
 def read_word_corpus(text_file = CORPUS_FILE):
     with open(text_file,'r') as f:
         return [x.strip() for x in f.readlines()]
 
-# Assumign that the Json object is stored as a list of strings, where each
-# string can be parsed out to be a dcionatry
-def read_json(text_file = JSON_FILE):
-    dictionaries = []
-    with open(text_file,'r') as f:
-        dic_str_list = f.readlines()
-        for dic_str in dic_str_list:
-            dictionaries.append(json.loads(dic_str))
-    return dictionaries
+def read_json(json_file):
+    print('...Loading in JSON...')
+    with open(json_file,'r') as f:
+        dictionary = json.load(f)
+    print('...Done...')
+    return dictionary
 
 class Feature_Extractor(object):
-    def __init__(self):
-        self.json_list = read_json()
-        self.word_corpus = read_word_corpus()
-        self.freq_matrix = np.zeros((len(self.json_list),len(self.word_corpus)))
-        # entry 1 is the price and entry 2 is the rating for each row which
-        # represents each wine
-        self.wine_features = np.zeros((len(self.json_list),2))
+    '''
+    Json List, Reviews, Word_Freq, Data_Features
+    Models: v (Dict Vetorizer), w (TfidfVectorizer)
+    '''
+    def __init__(self,json_file):
+        self.json_list = read_json(json_file)
+        self.reviews = []
+        print('...Cleaning Data...')
+        for json in self.json_list:
+            self.reviews.append(json['review'])
+            try:
+                del json['url']
+                del json['name']
+            except KeyError:
+                pass
+        print('...Parsing Data...')
+        self.v = DictVectorizer(sparse=True)
+        self.w = TfidfVectorizer(input='content')
+        self.word_freq = self.w.fit(self.reviews)
+        self.data_features = self.v.fit(self.json_list)
+        print('...Saving Data...')
+        self.save_matrix()
 
-    def count_freq(review):
-        len_words = len(review)
-        freq_array = np.zeros((1,len_words))
+    def save_matrix(self):
+        np.save('raw_features.npy',self.data_features)
+        np.save('word_freq.npy',self.word_freq)
+        pass
+
+    def get_feature_names(self):
+        return self.v.get_feature_names()
+
+    def get_word_names(self):
+        return self.w.get_feature_names()
+
+    '''
+    def count_freq(self,review):
+        freq_array = np.zeros((1,len(self.word_corpus)))
         for i, word in enumerate(review):
-            index = self.word_corpus.index(word)
-            freq_array[index] += 1
-        return freq_array/len_words
+            try:
+                index = self.word_corpus.index(word)
+                freq_array[index] += 1
+            except:
+                continue
+        return freq_array/len(review)
 
-    # For now these parse methods are written seperately for debugging pruposes,
-    # but should be combined in a latter version of the program for efficiency.
+    Old CODE
 
     def parse_reviews(self):
         for i,current_json in enumerate(self.json_list):
-            review_str = current_json[review] #review is the key of the review content
-            self.freq_matrix[i,:] = count_freq(review_str)
+            review_str = current_json['review'] #review is the key of the review content
+            if (i%5000 == 0):
+                print('Status: ', i)
+            if(len(review_str) > 0):
+                self.freq_matrix[i,:] = self.count_freq(review_str)
 
     def parse_features(self):
         for i,current_json in enumerate(self.json_list):
             price = int(current_json[price])
             rating = int(current_json[rating])
-            self.wine_features[i,;] = [price,rating]
-
-    def save_matrix(self):
-        np.save('frequency_matrix.npy',self.freq_matrix)
+            self.wine_features[i,:] = [price,rating
+    '''
