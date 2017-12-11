e2610acba98c0b45b667e6147ac3b7c03b4d4128
Richard Diehl Martinez <richarddiehl@gmail.com>
2017-12-11 02:49:07 -0800

    added features to cluster em

--- cluster_em.py
+++ cluster_em.py
@@ -15,10 +15,18 @@ class ClusterEM(object):
         self.covariances = None
         self.weights = None
 
-    def run_model(data):
+    def run_model(self,data):
         self.em = GaussianMixture(n_components = self.num_components).fit(data)
         self.covariances = self.em.covariances_
         self.means = self.em.means_
         self.weights = self.em.weights_
+        pass
 
-        
+    def get_means(self):
+        return self.means
+
+    def get_covariances(self):
+        return self.covariances
+
+    def get_weights(self):
+        return self.weights
