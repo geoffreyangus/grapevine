8240a10239ba9d286b77093ba342827e37327e64
Richard Diehl Martinez <richarddiehl@gmail.com>
2017-12-11 09:57:48 -0800

    changes to cluster and predictor

--- cluster_em.py
+++ cluster_em.py
@@ -15,16 +15,26 @@ import time
 def load_data(freq_data):
     return(sparse.load_npz(freq_data))
 
+'''
+The ClusterEM model stores the actual em model, as well as statistics
+derived from the ClusterEM model API.
+The model does not return to us the assignment probabilities for each data
+point to one of the clusters. So we also create a method to fill in this list.
+'''
+
 class ClusterEM(object):
     def __init__(self):
         self.em = None
+        self.data_matrix = None
         self.num_components = util.NUM_CLUSTERS
         self.means = None
         self.covariances = None
         self.weights = None
 
+        self.assignments = None
+
     def run_model(self,data=util.FREQ_DATA):
-        data_matrix = load_data(data).toarray()
+        self.data_matrix = load_data(data).toarray()
         #data_matrix = data_matrix[0:100,:]
         print('...Running EM Model...')
         start_time = time.time()
@@ -50,3 +60,15 @@ class ClusterEM(object):
 
     def predict_probs(self,data_point):
         return self.em.predict_proba()
+
+    def create_assignments(self,data=util.FREQ_DATA):
+        print('...Creating Assignments for EM Model ...')
+        # assuming that the data matrix has already been loaded in the run_model()
+        # step
+        self.assignments = np.zeros(self.data_matrix.shape)
+        for i in range(self.data_matrix.shape[0]):
+            self.assignments[i,:] = self.predict_probs(self.data_matrix[i,:])
+
+        # --------- Temporary Code -------
+        # Storing out matrix also so that we do not need to re-run algorithm
+        sparse.save_npz('em_assignments.npz',self.assignments)
--- predictor.py
+++ predictor.py
@@ -2,18 +2,80 @@
 Predictor.py
 -----------------------
 Input: Predictor Class returns the optimal wine that we predict for a user.
-Predictor relies on the output of cluster.py, in order to determine the
-clusters of wine. The Class then determines the optimal prediction by:
-    1) First determining the optimal cluster to sample different wines
-    2) Within this cluster finding the optimal wine by a process of linear
-    regression to find the wine with best quality for price.
+Predictor takes in a history of past wine recommendations as well as the
+unsupervised learning algorithm that clustered the wines. The algorithm
+then first hard clusters data (followed by soft-cluster depending on if
+EM algorithm was used as the unsupervised learning algorithm). Afterwards,
+the optimal wine is selected by maximizing a cost function.
 '''
 
 import numpy as np
+import random
+from scipy import sparse
+from scipy.sparse import csr_matrix
+
+'''
+History brought in as a File of JSON obects in the form of:
+
+{
+user_feedback: -1 | 1,
+cluster: [0, k]
+confidence: # always 1 if k-means, if EM prob returned
+features: [...]
+score: R
+price: R
+wine_id: MD5 hash
+}
+
+'''
+'''
+extra_cluster: returns the index of the cluster in which we want to begin
+our analysis of the ideal wine selection.s
+'''
+
+def extra_cluster(history,model):
+    num_clusters = model.num_clusters()
+
+    probs = np.zeros(num_clusters) #Final probabilities that we will return (multinomial)
+    pos = np.zeros(num_clusters)
+    neg = np.zeros(num_clusters)
+    num_wines = np.zeros(num_clusters)
+    denominator = 0
+    for wine in history:
+        cluster_assignment = wine['cluster']
+        if(user_feedback == 1):
+            pos[cluster_assignment] += 1
+        else
+            neg[cluster_assignment] += 1
+        num_wines[cluster_assignment] += 1
+
+        pos_total = np.sum(pos)
+        neg_total = np.sum(neg)
+        num_total = np.sum(num_wines)
+
+    probs = [num[k]*(pos[k]/pos_total)*(1-neg[k]/neg_total) for k in num_clusters]
+    probs = probs/sum(probs)
+    return random.choices(range(num_clusters),weights=probs, k=1)
 
 
 class Predictor(object):
-    def __init__(self,centroids,assignments):
+    def __init__(self):
+        pass
+
+    def predict(self,model,history):
+        cluster_index = extra_cluster(history)
+        wine_options = [] # Selecting the wine options from which we will
+                          # optimize to find the ideal wine
+        if(type(model) == ClusterEM):
+
+        else:
+            #NOTE: EM doesn't have assignments!
+            for i in range(len(model.assignments)):
+                if(model_assignments[i] == cluster_index):
+                    wine_options.append(i)
+
+
+
         self.centroids = centroids # a vector with all of the centroids
         self.assignments = assignments # a vector with all of the assingments
         # History of Recommendations
@@ -29,11 +91,15 @@ class Predictor(object):
         opt_wine = self.calculate_cost(opt_cluster)
         pass
 
-    # Algorithm that for a set of past wines predict where we will be
+    # Algorithm that for a set ofpast wines predict where we will be
     # drawing the new wine from. Looks over the entire history of wine
     # recommendations
 
     def find_cluster(self):
+
+
+
+
         a, num_clusters = np.unique(self.cluster_history,return_counts = True)
         cluster_size = np.zeros((num_clusters,1))
         cluster_response = np.zeros((num_clusters,1))
