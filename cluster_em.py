82d0d1be63747cf4224ed3f140a30f74c0a64459
Richard Diehl Martinez <richarddiehl@gmail.com>
2017-12-11 09:06:03 -0800

    em algorithm finished

--- cluster_em.py
+++ cluster_em.py
@@ -4,10 +4,16 @@ Code to cluster data using the EM model.
 
 import numpy as np
 from sklearn.mixture import GaussianMixture
+from scipy import sparse
+from scipy.sparse import csr_matrix
 import json
 import util
 import pickle
 import time
+
+# data file assumed to be in .npy file format
+def load_data(freq_data):
+    return(sparse.load_npz(freq_data))
 
 class ClusterEM(object):
     def __init__(self):
@@ -17,18 +23,21 @@ class ClusterEM(object):
         self.covariances = None
         self.weights = None
 
-    def run_model(self,data):
+    def run_model(self,data=util.FREQ_DATA):
+        data_matrix = load_data(data).toarray()
+        #data_matrix = data_matrix[0:100,:]
+        print('...Running EM Model...')
+        start_time = time.time()
+        self.em = GaussianMixture(n_components=self.num_components,covariance_type='spherical').fit(data_matrix)
         try:
-            print('...Running EM Model ...')
-            start_time = time.time()
-            self.em = GaussianMixture(n_components = self.num_components).fit(data)
             self.covariances = self.em.covariances_
             self.means = self.em.means_
             self.weights = self.em.weights_
             print('Clustering finished in: ', (time.time() - start_time))
         except:
             print('Data read in error')
-        pickle.dump(self.kmeans,open(util.OUTPUT_MODEL_EM,'wb'))
+        output_file = 'util.OUTPUT_MODEL_EM' + '_' + util.NUM_CLUSTERS
+        pickle.dump(self.em,open(output_file,'wb'))
 
     def get_means(self):
         return self.means
